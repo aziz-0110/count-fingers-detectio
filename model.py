99c67d0c4af7053cef7b10e30f6fd56d5caff4f0
Aziz <111878143+aziz-0110@users.noreply.github.com>
2024-08-28 14:32:01 +0700

    Update model.py

--- model.py
+++ model.py
@@ -1,7 +1,12 @@
 class Model:
+    def __init__(self):
+        from cvzone.HandTrackingModule import HandDetector
+
+        self.handdetec = HandDetector(maxHands=2)
+
     def count_finger(self, frame, tanda):
         import cv2
-        from cvzone.HandTrackingModule import HandDetector
+
         import cvzone
 
         angka = {
@@ -22,15 +27,16 @@ class Model:
 
         # tanda = input("Operasi : ")
 
-        handdetec = HandDetector(maxHands=2)
+
 
         # cap = cv2.VideoCapture(0)
         #
         # while True:
         #     ret, frame = cap.read()
         frame = cv2.flip(frame, 1)
+        # frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
 
-        hands, img = handdetec.findHands(frame, flipType=False)
+        hands, img = self.handdetec.findHands(frame, flipType=False)
 
         if hands:
             if len(hands) == 2:
@@ -38,8 +44,8 @@ class Model:
                 if hands[0]["type"] != hands[1]["type"]:
                     lmlist1 = hands[0] if hands[0]["type"] == "Left" else hands[1]
                     lmlist2 = hands[0] if hands[0]["type"] == "Right" else hands[1]
-                    fingerUp1 = handdetec.fingersUp(lmlist1)
-                    fingerUp2 = handdetec.fingersUp(lmlist2)
+                    fingerUp1 = self.handdetec.fingersUp(lmlist1)
+                    fingerUp2 = self.handdetec.fingersUp(lmlist2)
                     value1, value2 = 0, 0
                     for val, isi in angka.items():
                         if fingerUp1 == isi: value1 = int(val)
@@ -48,5 +54,5 @@ class Model:
 
                     cvzone.putTextRect(frame, f"{value1} {tanda} {value2} = {hasil}", pos=(20, 50), scale=3,
                                        thickness=3, colorR=(255, 0, 255), colorT=(255, 255, 255))
-        frame = cv2.resize(frame, (1000, 600))
+        frame = cv2.resize(frame, (1200, 1200))
         return frame
